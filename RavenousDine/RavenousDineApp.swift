ea12279c3a5af227ca39f43aa1aed3d64aed2be3
Rehnuma <rehnuma.reza@stitelnetworks.com>
2022-10-13 19:31:12 +0600

    Initial Commit

---
+++ RavenousDine/RavenousDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavenousDineApp.swift
+//  RavenousDine
+//
+//  Created by Rehnuma Reza on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RavenousDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
