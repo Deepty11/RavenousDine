ff83da67f7866d64cd881192c15b30af7dc909ad
Rehnuma <rehnuma.reza@stitelnetworks.com>
2022-10-13 20:16:30 +0600

    First Commit

--- RavenousDine/ContentView.swift
+++ RavenousDine/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self,
+                                  from: "menu.json")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            HStack {
+                                Image(item.thumbnailImage)
+                                    .clipShape(Circle())
+                                    .overlay(.gray, in: Circle().stroke(lineWidth: 2.0))
+                                VStack(alignment: .leading,
+                                       spacing: 1.0) {
+                                    Text(item.name)
+                                        .bold()
+                                    Text("$\(item.price)")
+                                        
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                }
+            }
+            .navigationTitle("Menu")
         }
-        .padding()
+        
     }
 }
 
